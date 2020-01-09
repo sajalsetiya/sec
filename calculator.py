a996690fdd257a692612cc8c63af034d22fd5f5c
[sajal setiya] <[sajalsetiya1998@gmail.com]>
2020-01-09 16:50:35 +0530

    one

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter your 1st no: "))
+num2 = int(input("Enter your 2st no: "))
+
+sum = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+
+
+print(sum)
+print(sub)
+print(multi)
+print(div)
